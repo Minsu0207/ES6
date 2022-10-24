e0a4889c089bfba0b174bdb53d6d21585d742e91
김민수 <min.su@daum.net>
2022-10-25 00:18:26 +0900

    css

---
+++ 04/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import WeatherMain from './com/WeatherDay/WeatherMain';
+import WeatherMain2 from './com/WeatherDay/WeatherMain2';
+// import '../src/com/Main.css';
+
+function App() {
+  return (
+    <div>
+      <WeatherMain />
+      <WeatherMain2 />
+
+    </div>
+  );
+}
+
+export default App;
